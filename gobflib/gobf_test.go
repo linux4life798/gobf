f8a6550e9ebab99e1ec9b7517d1c4e06f09836d7
Craig Hesling <craig@hesling.com>
2019-07-04 20:09:41 -0700

    Fix gobflib tests

--- gobflib/gobf_test.go
+++ gobflib/gobf_test.go
@@ -5,12 +5,16 @@ import (
 	"io"
 	"io/ioutil"
 	"os"
+	"os/exec"
+	"path/filepath"
 	"runtime"
 	"strings"
 	"testing"
 
 	"github.com/linux4life798/gobf/gobflib/lang"
 )
+
+const vec = true
 
 type testanspair struct {
 	name      string
@@ -65,31 +69,22 @@ var tests = []testanspair{
 	},
 }
 
-func GenerateNullReader() io.Reader {
-	return bytes.NewReader([]byte{})
+var testFiles = []string{}
+
+func init() {
+	var matches []string
+	matches, _ = filepath.Glob("../testprograms/*.b")
+	testFiles = append(testFiles, matches...)
+	matches, _ = filepath.Glob("../testprograms/*.bf")
+	testFiles = append(testFiles, matches...)
+	// matches, _ = filepath.Glob("../testprograms/bench/*.b")
+	// testFiles = append(testFiles, matches...)
+	// matches, _ = filepath.Glob("../testprograms/bench/*.bf")
+	// testFiles = append(testFiles, matches...)
 }
 
-func TestPrintStar(t *testing.T) {
-	bfcmds := strings.NewReader("++++++++++++++++++++++++++++++++++++++++++.... # ignore <.>[]+- everything here")
-	answerBuffer := []byte("****")
-	input := GenerateNullReader()
-	output := bytes.NewBuffer([]byte{})
-
-	prgm := NewIOBFProgram(1, 1, input, output)
-	prgm.ReadCommands(bfcmds)
-	err := prgm.Run()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if bytes.Compare(output.Bytes(), answerBuffer) != 0 {
-		t.Log("answer bytes:", answerBuffer)
-		t.Log("output bytes:", output.Bytes())
-		t.Fatal("Output does not match expected output")
-	}
-}
-
-func RunTest(t *testing.T, tpair *testanspair) {
+// RunTableTest is designed to run sub-tests
+func RunTableTest(t *testing.T, tpair *testanspair) {
 	bfcmds := strings.NewReader(tpair.cmds)
 	input := bytes.NewReader(tpair.input)
 	output := bytes.NewBuffer([]byte{})
@@ -125,10 +120,119 @@ func RunTest(t *testing.T, tpair *testanspair) {
 	}
 }
 
+// RunBenchCompile is designed to run sub-benchmarks of the compilation
+// process
+func RunBenchCompile(b *testing.B, tpair *testanspair, vectorize bool) {
+	for i := 0; i < b.N; i++ {
+		bfcmds := strings.NewReader(tpair.cmds)
+		input := bytes.NewReader(tpair.input)
+		output := bytes.NewBuffer([]byte{})
+
+		// Create program context and parse commands
+		prgm := NewIOBFProgram(0, 0, input, output)
+		prgm.ReadCommands(bfcmds)
+
+		ilb := prgm.CreateILTree()
+		ilb.Compress()
+		ilb.Prune()
+		if vectorize {
+			ilb.Vectorize()
+			ilb.VectorBalance()
+			ilb.Compress()
+			ilb.Prune()
+		}
+		if err, _ := lang.CompileIL(ilb, "/dev/null", false, false); err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+// RunBenchExecute is designed to run sub-benchmarks of the
+func RunBenchExecute(b *testing.B, tpair *testanspair, vectorize bool) {
+	const outbin = "/tmp/gobflib_bench"
+	bfcmds := strings.NewReader(tpair.cmds)
+	input := bytes.NewReader(tpair.input)
+	output := bytes.NewBuffer([]byte{})
+
+	// Create program context and parse commands
+	prgm := NewIOBFProgram(0, 0, input, output)
+	prgm.ReadCommands(bfcmds)
+
+	ilb := prgm.CreateILTree()
+	ilb.Compress()
+	ilb.Prune()
+	if vectorize {
+		ilb.Vectorize()
+		ilb.VectorBalance()
+		ilb.Compress()
+		ilb.Prune()
+	}
+	if err, _ := lang.CompileIL(ilb, outbin, false, false); err != nil {
+		b.Fatal(err)
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		cmd := exec.Command(outbin)
+		cmd.Stdout = ioutil.Discard
+		if err := cmd.Run(); err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
 func TestTable(t *testing.T) {
 	for i := range tests {
 		t.Run(tests[i].name, func(t *testing.T) {
-			RunTest(t, &tests[i])
+			RunTableTest(t, &tests[i])
+		})
+	}
+}
+
+func BenchmarkCompileTable(b *testing.B) {
+	for i := range tests {
+		b.Run(tests[i].name, func(b *testing.B) {
+			RunBenchCompile(b, &tests[i], vec)
+		})
+	}
+}
+
+func BenchmarkExecuteTable(b *testing.B) {
+	for i := range tests {
+		b.Run(tests[i].name, func(b *testing.B) {
+			RunBenchExecute(b, &tests[i], vec)
+		})
+	}
+}
+
+func BenchmarkCompileFiles(b *testing.B) {
+	for _, fname := range testFiles {
+		cmds, err := ioutil.ReadFile(fname)
+		if err != nil {
+			b.Fatal(err)
+		}
+		var t testanspair
+		t.name = filepath.Base(filepath.Dir(fname)) + "/" + filepath.Base(fname)
+		t.cmds = string(cmds)
+		t.input = []byte{}
+		b.Run(t.name, func(b *testing.B) {
+			RunBenchCompile(b, &t, vec)
+		})
+	}
+}
+
+func BenchmarkExecuteFiles(b *testing.B) {
+	for _, fname := range testFiles {
+		cmds, err := ioutil.ReadFile(fname)
+		if err != nil {
+			b.Fatal(err)
+		}
+		var t testanspair
+		t.name = filepath.Base(filepath.Dir(fname)) + "/" + filepath.Base(fname)
+		t.cmds = string(cmds)
+		t.input = []byte{}
+		b.Run(t.name, func(b *testing.B) {
+			RunBenchExecute(b, &t, vec)
 		})
 	}
 }
@@ -140,8 +244,8 @@ func TestNoProgramIL(t *testing.T) {
 	// Create program context and parse commands
 	prgm := NewIOBFProgram(0, 0, input, output)
 	il := prgm.CreateILTree()
-	il.Optimize()
-	lang.ILBlockToGo(il, output)
+	il.Compress()
+	lang.ILBlockToGo(il, output, false)
 }
 
 func BenchmarkParsingSource(b *testing.B) {
@@ -238,7 +342,7 @@ func BenchmarkReadFileAndRunPrintStar(b *testing.B) {
 
 func BenchmarkReadFileAndRunHelloWorld(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		cmdsfile, err := os.Open("../testprograms/printstar.b")
+		cmdsfile, err := os.Open("../testprograms/helloworld.b")
 		if err != nil {
 			b.Fatal(err)
 		}
